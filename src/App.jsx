129e4374517ed6f22e5b4c7858edd3b2cbe828b1
Jose7420 <7962446@alu.murciaeduca.es>
2024-01-14 18:58:03 +0100

    Add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Post from './componentes/Post'
+import { getAllPosts } from './servicios/posts/getAllPosts';
+import AjaxLoader from './componentes/AjaxLoader';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // Estado con la lista de post que recuperamos de la REST API
+  const [listaPost, setListaPost] = useState([]);
+  const [buscando, setbuscando] = useState(false);
+
+
+  function obtenerPosts() {
+    setbuscando(true);
+    
+
+    // Usamos el servicio de obtención de posts que hemos creado
+    getAllPosts().then(posts => {
+
+      //Cargamos los post en el estado del componente
+      setListaPost(posts);
+      setbuscando(false);
+
+
+
+    });
+
+  }
+
+  // Llamamos a la función de extracción de datos con un useEffect
+  // para que solo se ejecute una vez
+  useEffect(obtenerPosts, []);
+
+  // Función encargada de llamar al componente Post con el post
+  // que recibe como parámetro implícito de la función map
+  function muestraPost(post) {
+
+    return <Post key={post.id} post={post}></Post>;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+
+
+
+    <div id="divPost"> 
+      {buscando ? <AjaxLoader></AjaxLoader> :
+        <ul>
+          <h1>Post</h1>
+          {listaPost.map(muestraPost)}
+        </ul>
+      }
+    </div>
+  );
+}
+export default App;
---
+++ src/componentes/AjaxLoader.jsx
@@ -0,0 +1,13 @@
+import loading from '../img/loading.gif'
+
+
+const AjaxLoader = () => {
+
+    return (
+        <div>
+            <img src={loading} alt="loading"></img>
+        </div>
+    );
+}
+
+export default AjaxLoader;
