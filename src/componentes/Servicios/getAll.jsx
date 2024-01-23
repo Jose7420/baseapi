a9cf269c11580959ba4dcb67883af59c51a088d9
Jose7420 <7962446@alu.murciaeduca.es>
2024-01-23 10:16:50 +0100

    banderas

--- src/componentes/Servicios/getAll.jsx
+++ src/componentes/Servicios/getAll.jsx
@@ -5,7 +5,7 @@ export function getAllJuegos() {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': ,
+            'X-RapidAPI-Key': 'ffdbfc5751msh0bbc5f820324e10p13d625jsn3c49de84e2c3',
             'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
         }
     };
